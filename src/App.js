465b62d2af311cc49286afac3b080b6ff4cc8c94
clairecha7 <mybarclaire@gmail.com>
2023-05-17 23:02:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import data from "./helper/data";
+import List from "./components/List";
+import { useState } from "react";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [people, setPeople] = useState(data.slice(0, 5));
+  const handleNext = () => {
+    let newIndex = index + 5;
+    if (newIndex > data.length - 1) newIndex = 0;
+    setIndex(newIndex);
+    setPeople(data.slice(newIndex, newIndex + 5));
+  };
+  const handlePrev = () => {
+    let newIndex = index - 5;
+    if (newIndex < 0) newIndex = data.length - 5;
+    setIndex(newIndex);
+    setPeople(data.slice(newIndex, newIndex + 5));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>Employee List</h3>
+        <h5>
+          (Employees {index + 1} to {index + 5})
+        </h5>
+        <List people={people} />
+        <div>
+          <button onClick={handlePrev}>Prev</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
